bc547f325cb40bd40105ea632143e41504cef7da
lucero <48894073+luce0023@users.noreply.github.com>
2025-11-17 11:35:06 -0500

    Add files via upload

---
+++ ComprarProductosSeleccionados.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class ComprarProductosSeleccionados(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'')
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_comprar_productos_seleccionados(self):
+        driver = self.driver
+        driver.get("https://www.saucedemo.com/")
+        driver.find_element_by_id("user-name").click()
+        driver.find_element_by_id("user-name").clear()
+        driver.find_element_by_id("user-name").send_keys("standard_user")
+        driver.find_element_by_xpath("//div[@id='login_button_container']/div/form").click()
+        driver.find_element_by_id("password").click()
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("secret_sauce")
+        driver.find_element_by_id("login-button").click()
+        driver.find_element_by_link_text("1").click()
+        driver.find_element_by_id("checkout").click()
+        driver.find_element_by_id("first-name").click()
+        driver.find_element_by_id("first-name").clear()
+        driver.find_element_by_id("first-name").send_keys("abcde")
+        driver.find_element_by_id("last-name").click()
+        driver.find_element_by_id("last-name").clear()
+        driver.find_element_by_id("last-name").send_keys("abcde")
+        driver.find_element_by_id("postal-code").click()
+        driver.find_element_by_id("postal-code").clear()
+        driver.find_element_by_id("postal-code").send_keys("15304")
+        driver.find_element_by_id("continue").click()
+        driver.find_element_by_id("finish").click()
+        driver.find_element_by_id("back-to-products").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ LoginIncorrecto.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class LoginIncorrecto(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'')
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_login_incorrecto(self):
+        driver = self.driver
+        driver.get("https://www.saucedemo.com/")
+        driver.find_element_by_id("user-name").click()
+        driver.find_element_by_id("user-name").clear()
+        driver.find_element_by_id("user-name").send_keys("locked_out_user")
+        driver.find_element_by_id("password").click()
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("secreat_sauce")
+        driver.find_element_by_id("login-button").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
